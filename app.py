80bc262ca22783721cb149019bb8a7a0e52cf4d5
Loki <59907407+Loki-101@users.noreply.github.com>
2023-09-20 20:26:32 -0700

    Update app.py
    Undo last change, the table is already created in the egg install script.

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ async def startup_handler():
     db_host = os.getenv("DB_HOST", "Configure me first, moron!")
     db_name = os.getenv("DB_NAME", "license_db")
     db_port = int(os.getenv("DB_PORT", "3306"))
+    if os.getenv("LOG_LEVEL") == "DEBUG":
+        debug = True
     
     global pool
     pool = await aiomysql.create_pool(
@@ -24,14 +26,6 @@ async def startup_handler():
         user=db_user, password=db_password,
         db=db_name
     )
-    
-    async with pool.acquire() as conn:
-        async with conn.cursor() as cur:
-            await cur.execute("SHOW TABLES LIKE 'Licensing'")
-            result = await cur.fetchone()
-            if not result:
-                await create_licensing_table(cur)
-                
     print("Database connected")
 
 @app.shutdown_handler
@@ -40,17 +34,108 @@ async def shutdown_handler():
     pool.close()
     await pool.wait_closed()
 
-async def create_licensing_table(cur):
-    create_table_query = """
-    CREATE TABLE `Licensing` (
-      `license` VARCHAR(255) NOT NULL PRIMARY KEY,
-      `ip` VARCHAR(512) NOT NULL,
-      `discordId` VARCHAR(255) NOT NULL,
-      `email` VARCHAR(100) NOT NULL,
-      UNIQUE KEY `Licensing_license_ip_key` (`license`,`ip`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-    """
-    await cur.execute(create_table_query)
+@app.post("/")
+async def check_license(request):
+    if os.getenv("LOG_LEVEL").lower() == "debug":
+        debug = True
+    else:
+        debug = False
+    try:
+        # Extract the license_key from the JSON payload
+        data = json.loads(request.body)
+        license_key = data.get("license_key")
+        
+        headers_to_check = ["X-Forwarded-For", "Remote-Addr", "cf-connecting-ip"]
+
+        ip_address = None
+
+        for header in headers_to_check:
+            ip_address = request.headers.get(header)
+            if ip_address:
+                # If X-Forwarded-For contains multiple IPs (due to multiple proxies), take the first one
+                if header == "X-Forwarded-For" and "," in ip_address:
+                    ip_address = ip_address.split(",")[0].strip()
+                break
+
+        # If none of the headers provide the IP, use Robyn's method
+        if not ip_address:
+            ip_address = request.ip_addr
+
+        # Input validation
+        if not license_key or not ip_address:
+            return {"error": "Invalid input"}, 400
+
+        # Query to get the IP field for the given license_key
+        async with pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute(
+                    "SELECT ip FROM license_check WHERE license=%s",
+                    (license_key,)
+                )
+                result = await cur.fetchone()
+
+        # If there's a result and the extracted IP is in the list of IPs for that license
+        if result and ip_address in result[0].split(','):
+            valid = True
+        else:
+            valid = False
+
+        if debug:
+            print(f"IP Address: {ip_address}")
+            print(f"License Key: {license_key}")
+            print(f"Query Result: {valid}")
+        if valid:
+            if debug:
+                print("License verified")
+            response_data = {"status": "success", "message": "License verified."}
+            return json.dumps(response_data)
+        else:
+            if debug:
+                print("License not found or IP mismatch")
+            response_data = {"status": "failure", "message": "License not found or IP mismatch."}
+            return json.dumps(response_data)
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        print(f"Error occurred: {e}")
+        return {"error": "Internal Server Error"}, 500
+
+app.start(port=os.getenv("SERVER_PORT"))
+import os
+import json
+import aiomysql
+from robyn import Robyn
+
+os.environ["ROBYN_URL"] = "0.0.0.0"
+
+app = Robyn(__file__)
+
+pool = None
+
+@app.startup_handler
+async def startup_handler():
+    # Read configuration from environment variables
+    db_user = os.getenv("DB_USER", "username")
+    db_password = os.getenv("DB_PASSWORD", "password")
+    db_host = os.getenv("DB_HOST", "Configure me first, moron!")
+    db_name = os.getenv("DB_NAME", "license_db")
+    db_port = int(os.getenv("DB_PORT", "3306"))
+    if os.getenv("LOG_LEVEL") == "DEBUG":
+        debug = True
+    
+    global pool
+    pool = await aiomysql.create_pool(
+        host=db_host, port=db_port,
+        user=db_user, password=db_password,
+        db=db_name
+    )
+    print("Database connected")
+
+@app.shutdown_handler
+async def shutdown_handler():
+    global pool
+    pool.close()
+    await pool.wait_closed()
 
 @app.post("/")
 async def check_license(request):
